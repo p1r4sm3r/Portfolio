5167d0a02c505ab81a150f628357317401ec6042
Pirasmer <78717377+p1r4sm3r@users.noreply.github.com>
2022-07-20 00:07:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 var character = document.getElementById("character");
-    var block = document.getElementById("block");
-    var counter=0;
-    var highScore = 0;
-    var scores = [];
-    
+var block = document.getElementById("block");
+var counter=0;
+var highScore = 0;
+var scores = [];
+var button = document.getElementById('pause');
+var on = false;
+
+    button.addEventListener('click', function handleClick() {
+        if (on == true){
+            on = false;
+            block.style.animation = "none";
+            //character.classList.remove("animate");
+        }
+        else {
+            on = true
+            //character.classList.add("animate");
+            block.style.animation = "block 1s infinite linear";
+        };
+        console.log('on!', on);
+    });
     function jump(){
         if(character.classList == "animate"){return}
         character.classList.add("animate");
@@ -12,13 +27,14 @@ var character = document.getElementById("character");
         },300);
     }
     var checkDead = setInterval(function() {
+        if (on){
         let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
         let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
         if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
             block.style.animation = "none";
-            if (Math.floor(counter/100)>highScore){
-                highScore = Math.floor(counter/100);
-                scores.push(Math.floor(counter/100));
+            if (Math.floor(counter)>highScore){
+                highScore = Math.floor(counter);
+                scores.push(Math.floor(counter));
                 //exports.scores = scores;
                 document.getElementById("scores").value = scores
                 console.log(scores)
@@ -29,7 +45,7 @@ var character = document.getElementById("character");
         }
         else{
             counter++;
-            document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+            document.getElementById("scoreSpan").innerHTML = Math.floor(counter);
         }
-        
+    }
     }, 10);
